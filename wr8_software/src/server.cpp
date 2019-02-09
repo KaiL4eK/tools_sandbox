433bb51b9ca905f79add2e84cffe72019ed4c539
KaiL4eK <darklexi494@gmail.com>
2019-02-09 21:54:09 +0300

    GUI server integrated

--- wr8_software/src/server.cpp
+++ wr8_software/src/server.cpp
@@ -1,35 +1,22 @@
-#include <ros/ros.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Float32.h>
 
-#include <svserver.h>
-
-SVServer *server = nullptr;
-
-void topicCallback(const std_msgs::Float32& msg)
-{
-	ROS_INFO("Received potentiometer data: %d", msg.data);
-
-	server->sendData( SVServer::State::WAIT, 20, msg.data, 50, 55);
-}
-
-#include "svserver.h"
-#include "datapackage.h"
+#include <gui_server.hpp>
 
 int main(int argc, char **argv)
 {
+	/* ROS routine */
 	ros::init(argc, argv, "gui_server");
 
-	ros::NodeHandle n;
+	/* Qt server routine */	
+	QCoreApplication app(argc, argv);
+	qRegisterMetaType<DataPackage>();
 
-	server = new SVServer();
-	// server->start(QHostAddress("0.0.0.0"), 5555);
-	server->start(QHostAddress("192.168.31.130"), 5555);
+	SVServer server;
+	server.start( QHostAddress("192.168.31.152"), 5556 );
 
-	ros::Subscriber sub = n.subscribe("cpp_chatter", 1000, topicCallback);
+	ROSServer ros_server(&server);
+	ros_server.start();
 
+	QObject::connect( &ros_server, SIGNAL(rosShutdown()), &app, SLOT(quit()), Qt::QueuedConnection );
 
-	ros::spin();
-
-	return 0;
+    return app.exec();
 }

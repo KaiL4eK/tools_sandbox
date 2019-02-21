ba1e38058aa447f0745e3a5951ee510d0cef627e
Alexey <nine-4lex@mail.ru>
2019-02-21 18:43:27 +0300

    [wr8_soft] Encoder + steer

--- wr8_gui_server/include/gui_server.hpp
+++ wr8_gui_server/include/gui_server.hpp
@@ -1,40 +1,58 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Int32.h>
 
 #include <svserver.h>
 
 #include <QCoreApplication>
 #include <QObject>
+#include <QTimer>
 
 class ROSServer : public QThread
 {
 	Q_OBJECT
 
 	SVServer *m_server;
+	QTimer	 sendTmr;
+
+	DataPackage	m_dataPkg;
 
 public:
     ROSServer(SVServer *server)
     {
     	m_server = server;
+    	m_dataPkg = DataPackage( DataPackage::State::WAIT );
 
     	QObject::connect( this, SIGNAL(sendData(DataPackage const&)), server, SLOT(sendData(DataPackage const&)) );
+    	
+    	QObject::connect( &sendTmr, SIGNAL(timeout()), this, SLOT(sendTmrHandler()) );
+    	
+    	sendTmr.start(200);
     }
 
-	void topicCallback(const std_msgs::Float32& msg)
+    void sendTmrHandler()
+    {
+		Q_EMIT sendData( m_dataPkg );
+    }
+
+	void encoderRawCb(const std_msgs::Float32& msg)
 	{
 		ROS_INFO("Received potentiometer data: %g", msg.data);
-		
-		DataPackage data( DataPackage::State::WAIT );
-		data.m_steeringAngle = msg.data;
+		m_dataPkg.m_steeringAngle = msg.data;
+	}
 
-		emit sendData( data );
+	void steerCb(const std_msgs::Int32& msg)
+	{
+		ROS_INFO("Received potentiometer data: %d", msg.data);
+		m_dataPkg.m_encoderValue = msg.data;
 	}
 
 	void run()
 	{
 		ros::NodeHandle n;
-		ros::Subscriber sub = n.subscribe("steering", 1000, &ROSServer::topicCallback, this);
+		n.subscribe("steering", 1000, &ROSServer::steerCb, this);
+		n.subscribe("encoder_raw", 1000, &ROSServer::encoderRawCb, this);
 		
 		while ( ros::ok() )
 		{

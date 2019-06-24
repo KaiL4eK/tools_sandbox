3f1a8c7ff1e2d5c14610b57d33e09ba65f354bd6
KaiL4eK <darklexi4@mail.ru>
2019-06-24 22:02:41 +0300

    Last tests

--- gst_mjpeg.sh
+++ gst_mjpeg.sh
@@ -66,6 +66,10 @@ exit
 
 
 # UDP
+gst-launch-1.0 -v videotestsrc ! "video/x-raw,width=640, height=480, framerate=30/1" ! videoconvert ! \
+    x264enc tune=zerolatency bitrate=5000 speed-preset=superfast ! \
+    rtph264pay pt=96 ! udpsink host=192.168.31.76 port=$GST_PORT
+
 gst-launch-1.0 -v v4l2src device=$GST_DEVICE ! "video/x-raw,width=640,framerate=30/1" ! videoconvert ! \
     x264enc tune=zerolatency bitrate=5000 speed-preset=superfast ! \
     rtph264pay pt=96 ! udpsink host=192.168.31.76 port=$GST_PORT

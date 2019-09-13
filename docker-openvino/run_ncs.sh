b0f4bd3b13ce80a05e901c8c7b844ebd58c8e35a
KaiL4eK <darklexi494@gmail.com>
2019-09-13 20:08:59 +0300

    [docker-openvino]: Removed

--- docker-openvino/run_ncs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it --privileged \
-    -v /dev:/dev \
-    docker-openvino
--- docker-openvino/run_ncs_ws.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it --privileged \
-    -v /dev:/dev -v `pwd`:/home/developer/ws \
-    docker-openvino
